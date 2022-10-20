899345f9facee2b208d89ed35e5ac568ec6031bd
ita-jp <tn00gm@gmail.com>
2022-10-20 11:47:41 +0900

    Show Header in top page.

--- character-list/src/App.tsx
+++ character-list/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Heading } from '@chakra-ui/react';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className='container'>
+    <Heading size="lg" as="h1" my={12}>
+      登場人物一覧
+    </Heading>
+  </div>
+  )
 }
 
 export default App;
